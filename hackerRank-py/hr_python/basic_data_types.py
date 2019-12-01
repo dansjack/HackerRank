8d24cb2e289a6ac7b177f5951ddfc9406a9554c3
dsjack <dsjack89@gmail.com>
2019-12-01 12:26:54 -0800

    Adds solution and test for minion_game()

--- hackerRank-py/hr_python/basic_data_types.py
+++ hackerRank-py/hr_python/basic_data_types.py
@@ -1,3 +1,21 @@
+
+
 def list_comprehensions(x, y, z, n):
     return [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in
             range(z + 1) if ((i + j + k) != n)]
+
+
+def minion_game(s):
+    scores = {'stuart': 0, 'kevin': 0}
+    for i in range(len(s)):
+        if s[i] in 'AEIOU':
+            scores['kevin'] += len(s) - i
+        else:
+            scores['stuart'] += len(s) - i
+
+    if scores['kevin'] > scores['stuart']:
+        ('{} {}'.format('Kevin', scores['kevin']))
+    elif scores['kevin'] < scores['stuart']:
+        print('{} {}'.format('Stuart', scores['stuart']))
+    else:
+        print('Draw')
--- hackerRank-py/tests/test_hr_python/test_basic_data_types.py
+++ hackerRank-py/tests/test_hr_python/test_basic_data_types.py
@@ -1,6 +1,8 @@
 import unittest
+from io import StringIO
+from unittest.mock import patch
 
-from hr_python.basic_data_types import list_comprehensions
+from hr_python.basic_data_types import list_comprehensions, minion_game
 
 
 class TestBasicDataTypes(unittest.TestCase):
@@ -20,3 +22,12 @@ class TestBasicDataTypes(unittest.TestCase):
              [1, 2, 0], [1, 2, 1], [1, 2, 2], [2, 0, 1], [2, 0, 2],
              [2, 1, 0], [2, 1, 1], [2, 1, 2], [2, 2, 0], [2, 2, 1],
              [2, 2, 2]], list_comprehensions(2, 2, 2, 2))
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_minion_game(self, mock_stdout):
+        """
+        Tests the appropriate name and value are returned
+        """
+        minion_game('BANANA')
+        minion_game('BANANANAAAS')
+        assert mock_stdout.getvalue() == 'Stuart 12\nDraw\n'
