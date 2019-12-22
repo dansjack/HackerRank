bf208d51861755247c75ebe7b6d613f8894c0978
dsjack <dsjack89@gmail.com>
2019-12-21 17:01:27 -0800

    Adds solution for Left Rotation problem

--- hackerRank-j/src/dataStructures/HR_Arrays.java
+++ hackerRank-j/src/dataStructures/HR_Arrays.java
@@ -2,12 +2,14 @@ package dataStructures;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class HR_Arrays {
 
   public static void main(String[] args) {
-    arraysDS();
+
 
 
   }
@@ -24,6 +26,22 @@ public class HR_Arrays {
       System.out.print(nums[i] + " ");
     }
 
+  }
+
+  public static void leftRotation() {
+    Scanner reader = new Scanner(System.in);
+    int n = reader.nextInt();
+    int d = reader.nextInt();
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < n; i++) {
+      q.add(reader.nextInt());
+    }
+    for (int i = 0; i < d; i++) {
+      q.add(q.remove());
+    }
+    for (int i = 0; i < n; i++) {
+      System.out.print(q.remove() + " ");
+    }
   }
 
 }
